d8bd66f1d381444b9f5a0d000462791266a737e9
Typho7 <caro.d@free.fr>
2023-09-12 15:50:04 +0200

    Add function to check date, checkbox and radio btn

--- modal.js
+++ modal.js
@@ -1,5 +1,4 @@
-/** Cette fonction gère le basculement en responsive du menu du header
- * @param {} : aucun
+/** This function is used to toggle the header in responsive
  */
 function editNav() {
   const topNav = document.getElementById("myTopnav");
@@ -16,28 +15,43 @@ const modalBtn = document.querySelector(".modal-btn");
 const formData = document.querySelector(".form-data");
 const closeBtn = document.querySelector(".close")
 const form = document.querySelector("form")
-const tagFirst = document.querySelector("#first")
-const messageFirst = document.querySelector("#message-first")
-const tagLast = document.querySelector("#last")
-const messageName = document.querySelector("#message-name")
-const tagEmail = document.querySelector("#email")
-const messageMail = document.querySelector("#message-mail")
-const tagBirthdate = document.querySelector("#birthdate")
-const messageDate = document.querySelector("#message-date")
-const tagQuantity = document.querySelector("#quantity")
-const tagLocation = document.querySelectorAll("input[name='location']")
-const tagCheckbox1 = document.querySelector("#checkbox1")
-const tagCheckbox2 = document.querySelector("#checkbox2")
+let tagFirst = document.querySelector("#first")
+let messageFirst = document.querySelector("#message-first")
+let tagLast = document.querySelector("#last")
+let messageName = document.querySelector("#message-name")
+let tagEmail = document.querySelector("#email")
+let messageMail = document.querySelector("#message-mail")
+let tagBirthdate = document.querySelector("#birthdate")
+let messageDate = document.querySelector("#message-date")
+let tagQuantity = document.querySelector("#quantity")
+let messageQuantity = document.querySelector("#message-quantity")
+let tagLocation = document.getElementsByName("location")
+let messageLocation = document.querySelector("#message-location")
+let tagCheckbox = document.querySelector("#checkbox1")
+let messageCG = document.querySelector("#message-cg")
+let tagCheckbox2 = document.querySelector("#checkbox2")
+
+// ----- Other Variable ------
+const currentDate = new Date()
 const formErrors = { shortName : "Il faut au minimum 2 charactères.",
                    badName : "Un caractère n'est pas reconnu. Merci de corriger.",
                    badEmail : "Veuillez renseigner une adresse mail valide.",
                    tooYoung : "Vous devez avoir plus de 18 ans pour participer",
-                   afterToday : "Etes vous vraiment né dans le futur ?",
-                   veryElder : "Merci de corriger votre année de naissance",
+                   futureMan : "Etes vous vraiment né dans le futur ?",
                    badDate : "Veuillez renseigner une date valide.",
                    badQuantity : "Veuillez renseigner un nombre.",
                    noCity : "Merci de selectionner une ville.",
-                   RefuseCG : "Vous devez accepter les Conditions générales de participation."}  
+                   refuseCG : "Merci d'accepter les conditions d'utilisation."}  
+
+                   
+// ------ Regular Expression RegEx ------
+// Accepte les majuscules, minuscules, caractères accentués, espace (\s) aprostrophes et tirets
+const nameFirstRegEx = new RegExp("^[A-Za-zÀ-ÖØ-öø-ÿ\s'-]+$") 
+const emailRegEx = new RegExp("^[a-zA-Z0-9._-]{2,}@[a-zA-Z0-9.-]{2,}\.[a-zA-Z]{2,}$")
+const twoOrMoreRegEx = /.{2,}/;
+const numberRegEx = /^\d+$/;
+// de 0 à 9 puis 10 à 29 puis 30 et 31 / mois / puis année commence par 19 ou 20
+const dateRegEx = new RegExp("^(19|20)\\d\\d-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$") 
 
 // ------ Launch modal event ------ 
 modalBtn.addEventListener("click", launchModal);  
@@ -47,7 +61,7 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-// ------ close modal event ------ 
+// ------ Close modal event ------ 
 closeBtn.addEventListener("click", closeModal);
 
 // This function hide the modal 
@@ -55,18 +69,12 @@ function closeModal() {
   modalbg.style.display = "none"
 }
 
-// ------ Expression régulière RegEx ------
-// Accepte les majuscules, minuscules, caractères accentués, espace (\s) aprostrophes et tirets
-const nameFirstRegEx = new RegExp("^[A-Za-zÀ-ÖØ-öø-ÿ\s'-]+$") 
-const emailRegEx = new RegExp("^[a-zA-Z0-9._-]{2,}@[a-zA-Z0-9.-]{2,}\.[a-zA-Z]{2,}$")
-const twoOrMoreRegEx = /.{2,}/;
-const numberRegEx = /^\d+$/;
-// de 0 à 9 puis 10 à 29 puis 30 et 31 / mois / puis année commence par 19 ou 20
-const dateRegEx = new RegExp("^(19|20)\\d\\d-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$") 
-
 /** This function check that the name / firstname are 
  *  - using normal letters 
- *  - have more than 2 letters  
+ *  - have more than 2 letters
+ *  Received two parameters : 
+ * {firstOrName} Name or FirstName 
+ * {messageLoc} output message location  
  */
 function checkName(firstOrName,messageLoc) {
   if (!twoOrMoreRegEx.test(firstOrName)) {
@@ -90,53 +98,72 @@ function checkEmail(email) {
 }
 
 /**
- * Cette fonction prend la date de naissance en paramètre.
- * Elle valide qu'elle est au bon format. 
+ * This function check that the birthdate :
+ * - use normal numbers
+ * - the user is not under 18 years old
+ * - the user birthday is not after current date
  */
 function checkBDate(date) {
+  // Extract user year
+  let userDateSplit = date.split("-");
+  let userYear = userDateSplit[0];
+
+  // Extract current year
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
+  // Age of the user calculation
+  let userAge = currentYear - userYear
+
   if (!dateRegEx.test(date)) {
     messageDate.innerText = formErrors.badDate
-  }
-  if (!twoOrMoreRegEx.test(date)) {
-    messageDate.innerText = formErrors.badDate
+  } else if ( userAge < 0 ) {
+    messageDate.innerText = formErrors.futureMan
+  } else if ( userAge < 18 ) {
+    messageDate.innerText = formErrors.tooYoung
+  } else {
+    messageDate.innerText = "";
   }
 }
 
-/**
- * Cette fonction prend la date de naissance en paramètre.
- * Elle valide que l'individu a plus de 18 ans.
- * N'a pas plus de 130 ans.
- * N'est pas né après la date du jour.
-
-function validerAge(date) {
-  if (!dateRegEx.test(date)) {
-     new Error("Votre date de naissance n'est pas valide.")
-  }
-} */
+/** This function check that the participation is a number.
+*/
 function checkQuantity(quantity) {
   if (!numberRegEx.test(quantity)) {
-      console.log("erreur nombre")
-      throw new Error("Votre nombre de participation n'est pas un nombre.")
+    messageQuantity.innerText = formErrors.badQuantity
+  } else {
+    messageQuantity.innerText = "";
   }
 }
 
-/**
- * Cette fonction vérifie que les condition générales sont acceptées.
- * @param {string} checkbox1 
- * @throws {Error}
+/** This function check if the term and conditions box is checked.
  */
-function validerCG(checkbox) {
+function checkCG(checkbox) {
+  console.log("lacheckbox est ",checkbox)
   if (!checkbox.checked) {
-    console.log("erreur CG")
-    throw new Error("Vous n'avez pas acceptez les conditions d'utilisations.")
+    console.log("ma checkbox vaut", checkbox.checked)
+    messageCG.innerText = formErrors.refuseCG
+  } else {
+    messageCG.innerText = "";
   }
 }
 
-/**
- * Cette fonction vérifie qu'une ville est choisie.
- * @param {string} checkbox1 
- * @throws {Error}
+/** This function ckeck is a city is chosen.
  */
+function checkLocation(location) {
+  let isChecked = false;
+  for (let i = 0; i < location.length; i++) {
+    if (location[i].checked) {
+      isChecked = true;
+      break;
+    }
+  }
+  if (isChecked) {
+    messageLocation.innerText = "";
+  } else {
+    messageLocation.innerText = formErrors.noCity;
+  }
+}
 
 
 // ------ function Submit ------ 
@@ -149,14 +176,17 @@ form.addEventListener("submit", (event) => {
     console.log(birthdate)
   const quantity = tagQuantity.value
     console.log(quantity)
-  /*const checkbox1 = tagCheckbox1.checked
-  const form = document.querySelector("form")*/
+
   
   try {
     // 1st form control on submission event
     checkName(tagFirst.value,messageFirst);
     checkName(tagLast.value,messageName);
     checkEmail(tagEmail.value);
+    checkBDate(tagBirthdate.value);
+    checkQuantity(tagQuantity.value);
+    checkCG(tagCheckbox);
+    checkLocation(tagLocation);
 
     // Submission control on change event
     tagFirst.addEventListener('change', () => {
@@ -168,30 +198,34 @@ form.addEventListener("submit", (event) => {
     tagEmail.addEventListener('change', () => {
       checkEmail(tagEmail.value);
     });
- 
-    
+    tagBirthdate.addEventListener('change', () => {
+      checkBDate(tagBirthdate.value);
+    });
+    tagQuantity.addEventListener('change', () => {
+      checkQuantity(tagQuantity.value);
+    });
+    tagCheckbox.addEventListener('change', () => {
+      checkCG(tagCheckbox);
+    });
+    tagLocation.forEach((isNewLocationChecked) => isNewLocationChecked.addEventListener('change', function() {
+      checkLocation(); 
+    }));
 
-    checkBDate(birthdate)
-      console.log("birthdate OK")
-
-      console.log("email OK")
-    checkQuantity(quantity)
-      console.log("nombre OK")
-    /* validerCG(checkbox1)
-    console.log("CG checkbox ok")
-    form.submit;*/
       console.log("Soumission OK")  
 
 
-      const submissionForm = { fistName : tagFirst.value,
+    const submissionForm = { 
+        fistName : tagFirst.value,
         lastName : tagLast.value,
         email : tagEmail.value,
         birthdate : tagBirthdate.value,
         numberParticipation : tagQuantity.value,
-        newsletter : tagCheckbox2.checked ? "accept" : "Refused"
+        Terms : tagCheckbox.checked ? "Accept" : "Refused",
+        newsletter : tagCheckbox2.checked ? "Accept" : "Refused"
        }
-  } catch(Error) {
-      console.log("Erreur a été Catch")
+       
+  } catch(error) {
+      console.error("Erreur lors de la soumission du formulaire")
   }
 }
 );
